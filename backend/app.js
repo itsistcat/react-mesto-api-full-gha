80b3f32fb299c309672c21173d229b4de9ad49e7
Olga Chumachenko <itsistcat@gmail.com>
2023-10-31 17:36:03 +0200

    app

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const NotFoundError = require('./errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
 
 const URL = 'mongodb://localhost:27017/mestodb';
-const { PORT = 3001 } = process.env;
+const { PORT = 3000 } = process.env;
 
 mongoose.set('strictQuery', true);
 mongoose.connect(URL);
