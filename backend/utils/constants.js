d7c32c353bff2fe06da71d262dee677e3b5c69a5
Olga Chumachenko <itsistcat@gmail.com>
2023-11-02 19:57:13 +0200

    slomaloc

--- backend/utils/constants.js
+++ backend/utils/constants.js
@@ -1,5 +1,5 @@
 const { NODE_ENV } = process.env;
-const { SECRET_SIGNING_KEY = 'U2FsdGVkX19nV2KreWqHk1BGD+ojOGgl39N93rtj1DVVSeYcdNPGnAQt4PtU2FvJrEiPtoQRACJ0B/yIORhjvQ==' } = process.env;
+const { SECRET_SIGNING_KEY } = process.env;
 const { MONGODB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const URL_REGEX = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/;
